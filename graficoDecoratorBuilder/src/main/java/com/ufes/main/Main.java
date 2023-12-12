2276be165c26f944f3660a9ce44464cb891962e6
talleshts <talles.santos@itixti.com.br>
2023-12-12 14:35:21 -0300

    Criando projeto

---
+++ graficoDecoratorBuilder/src/main/java/com/ufes/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ufes.main;
+
+/**
+ *
+ * @author tallesh
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
